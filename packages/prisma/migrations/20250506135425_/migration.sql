9fcf08397cc0b537ee305e5e0e308f0754093a12
Lawrence Lin <deduce@gmail.com>
2025-05-06 12:56:03 -0500

    chore: update dependencies and configurations

---
+++ packages/prisma/migrations/20250506135425_/migration.sql
@@ -0,0 +1,158 @@
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` VARCHAR(191) NOT NULL,
+    `slug` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `content` TEXT NULL,
+    `excerpt` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `categoryId` VARCHAR(191) NOT NULL,
+    `siteId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Post_slug_key`(`slug`),
+    INDEX `Post_categoryId_idx`(`categoryId`),
+    INDEX `Post_siteId_idx`(`siteId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Category` (
+    `id` VARCHAR(191) NOT NULL,
+    `slug` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `postsCount` INTEGER NOT NULL DEFAULT 0,
+    `siteId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Category_slug_key`(`slug`),
+    INDEX `Category_slug_idx`(`slug`),
+    INDEX `Category_siteId_idx`(`siteId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `user` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` TEXT NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `emailVerified` BOOLEAN NOT NULL,
+    `image` TEXT NULL,
+    `createdAt` DATETIME(3) NOT NULL,
+    `updatedAt` DATETIME(3) NOT NULL,
+    `role` TEXT NULL,
+    `banned` BOOLEAN NULL,
+    `banReason` TEXT NULL,
+    `banExpires` DATETIME(3) NULL,
+
+    UNIQUE INDEX `user_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `session` (
+    `id` VARCHAR(191) NOT NULL,
+    `expiresAt` DATETIME(3) NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL,
+    `updatedAt` DATETIME(3) NOT NULL,
+    `ipAddress` TEXT NULL,
+    `userAgent` TEXT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `impersonatedBy` TEXT NULL,
+    `activeOrganizationId` TEXT NULL,
+
+    INDEX `session_userId_idx`(`userId`),
+    UNIQUE INDEX `session_token_key`(`token`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `account` (
+    `id` VARCHAR(191) NOT NULL,
+    `accountId` TEXT NOT NULL,
+    `providerId` TEXT NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `accessToken` TEXT NULL,
+    `refreshToken` TEXT NULL,
+    `idToken` TEXT NULL,
+    `accessTokenExpiresAt` DATETIME(3) NULL,
+    `refreshTokenExpiresAt` DATETIME(3) NULL,
+    `scope` TEXT NULL,
+    `password` TEXT NULL,
+    `createdAt` DATETIME(3) NOT NULL,
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `account_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `verification` (
+    `id` VARCHAR(191) NOT NULL,
+    `identifier` TEXT NOT NULL,
+    `value` TEXT NOT NULL,
+    `expiresAt` DATETIME(3) NOT NULL,
+    `createdAt` DATETIME(3) NULL,
+    `updatedAt` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `organization` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` TEXT NOT NULL,
+    `slug` VARCHAR(191) NOT NULL,
+    `logo` TEXT NULL,
+    `createdAt` DATETIME(3) NOT NULL,
+    `metadata` TEXT NULL,
+
+    UNIQUE INDEX `organization_slug_key`(`slug`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `member` (
+    `id` VARCHAR(191) NOT NULL,
+    `organizationId` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `role` TEXT NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL,
+
+    INDEX `member_organizationId_idx`(`organizationId`),
+    INDEX `member_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `invitation` (
+    `id` VARCHAR(191) NOT NULL,
+    `organizationId` VARCHAR(191) NOT NULL,
+    `email` TEXT NOT NULL,
+    `role` TEXT NULL,
+    `status` TEXT NOT NULL,
+    `expiresAt` DATETIME(3) NOT NULL,
+    `inviterId` VARCHAR(191) NOT NULL,
+
+    INDEX `invitation_organizationId_idx`(`organizationId`),
+    INDEX `invitation_inviterId_idx`(`inviterId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Site` (
+    `id` VARCHAR(191) NOT NULL,
+    `subdomain` VARCHAR(191) NOT NULL,
+    `domain` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `organizationId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Site_subdomain_key`(`subdomain`),
+    UNIQUE INDEX `Site_domain_key`(`domain`),
+    INDEX `Site_subdomain_idx`(`subdomain`),
+    INDEX `Site_domain_idx`(`domain`),
+    INDEX `Site_organizationId_idx`(`organizationId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
