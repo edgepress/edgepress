964e2fa172c9a6eff9d4e33b70eea31639ff1fbe
Lawrence Lin <deduce@gmail.com>
2025-05-18 23:18:46 -0500

    feat: add emoji support to markdown plugin

--- packages/ui/src/components/editor/plugins/markdown-plugin.tsx
+++ packages/ui/src/components/editor/plugins/markdown-plugin.tsx
@@ -6,12 +6,19 @@ import {
   remarkMention,
 } from '@udecode/plate-markdown';
 import { SuggestionPlugin } from '@udecode/plate-suggestion/react';
+import remarkEmoji from 'remark-emoji';
 import remarkGfm from 'remark-gfm';
 import remarkMath from 'remark-math';
 
 export const markdownPlugin = MarkdownPlugin.configure({
   options: {
     disallowedNodes: [SuggestionPlugin.key],
-    remarkPlugins: [remarkMath, remarkGfm, remarkMdx, remarkMention],
+    remarkPlugins: [
+      remarkMath,
+      remarkGfm,
+      remarkMdx,
+      remarkMention,
+      remarkEmoji as any,
+    ],
   },
 });
